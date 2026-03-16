1a92dee94d8b2195bc66ef03eb4bbead78e49097
dannabetancur <dannarojasb221@gmail.com>
2026-03-16 10:43:52 +0100

    Implement update test #3

---
+++ Tests/CITests/UpdateTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using DbManager.Security;
+using DbManager;
+using DbManager.Parser;
+
+namespace SecurityParsingTests
+{
+    public class UpdateTests
+    {
+        [Fact]
+        public void updateTableDontExist()
+        {
+            Database db = Database.CreateTestDatabase();
+            List<SetValue> setValues = new List<SetValue>() { new SetValue("Name", "Fiona") };
+            Update updateQuery = new Update("TablaInventada", setValues, null);
+            string result = updateQuery.Execute(db);
+            Assert.Equal(Constants.TableDoesNotExistError, result);
+        }
+        [Fact]
+        public void
+        
+
+    }
+}
